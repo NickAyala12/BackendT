b0309df0296690ceb5e03405dc5f472c7b6789d8
NickAyala12 <165934404+NickAyala12@users.noreply.github.com>
2026-06-04 17:19:21 -0500

    Initialize Spring Boot Backend with Maven

---
+++ src/main/java/com/turismobarranca/backendt/BackendtApplication.java
@@ -0,0 +1,13 @@
+package com.turismobarranca.backendt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turismobarranca/backendt/BackendtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismobarranca.backendt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
