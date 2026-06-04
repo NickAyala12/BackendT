0600c80b29c7be0d693e5f3584fbcc0ab15048c9
NickAyala12 <165934404+NickAyala12@users.noreply.github.com>
2026-06-04 17:30:00 -0500

    config: add swagger dependency and webconfig for cross-origin resources

---
+++ src/main/java/com/turismobarranca/backendt/config/webconfig.java
@@ -0,0 +1,25 @@
+package com.turismobarranca.backendt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webconfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Permite que tu FrontendT (React) se conecte a cualquier endpoint de la API
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Puerto por defecto de Vite
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
